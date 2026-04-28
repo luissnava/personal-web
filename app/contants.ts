75ec4bb29efc7b786ecdf55024dd883884b1a732
navitacode <nhokahey@gmail.com>
2026-04-27 21:20:47 -0600

    images to webp

--- app/contants.ts
+++ app/contants.ts
@@ -4,7 +4,7 @@ export const projects = [
         title: "Mi Comanda POS",
         description:
             "Responsable del desarrollo de un sistema pos multiplataforma que corre en cualquier dispositivo: Android, iOS y web. Permite a negocios gestionar ventas, inventario y comandas desde cualquier lugar sin depender de hardware especializado.",
-        image: "/assets/images/micomandapos.png",
+        image: "/assets/images/micomandapos.webp",
         tags: ["TypeScript", "React", "Next.js", "Context API", "TailwindCSS"],
         link: "https://micomandapos.gruponava.org",
         featured: true,
@@ -15,7 +15,7 @@ export const projects = [
         title: "Kachimushi",
         description:
             "Responsable del desarrollo del sitio web. Resuelve problemas de posicionamiento de marca, embudo de ventas y atención al cliente, proporcionando una vitrina digital que conecta al negocio con su audiencia.",
-        image: "/assets/images/kachimushi.png",
+        image: "/assets/images/kachimushi.webp",
         tags: ["HTML", "CSS", "Java script", "JQuery"],
         link: "https://kachimushi.com/",
         featured: false,
@@ -26,7 +26,7 @@ export const projects = [
         title: "C4POS",
         description:
             "Responsable del desarrollo del sitio web. Resuelve problemas de posicionamiento de marca, embudo de ventas y atención al cliente, facilitando la captación de clientes y fortaleciendo la presencia digital del negocio.",
-        image: "/assets/images/c4pos.png",
+        image: "/assets/images/c4pos.webp",
         tags: ["HTML", "CSS", "Java Script", "JQuery"],
         link: "https://c4pos.com/",
         featured: false,
@@ -37,7 +37,7 @@ export const projects = [
         title: "Ipesa Pinturas",
         description:
             "Responsable del desarrollo del sitio web corporativo. Resuelve problemas de posicionamiento de marca, embudo de ventas y la automatizacion de atención al cliente, ofreciendo una experiencia digital que conecta a la empresa con sus clientes de forma directa y efectiva.",
-        image: "/assets/images/ipesa.png",
+        image: "/assets/images/ipesa.webp",
         tags: ["TypeScript", "React", "Next.js", "Context API", "TailwindCSS"],
         link: "https://www.ipesamex.com",
         featured: true,
@@ -49,7 +49,7 @@ export const projects = [
         title: "Aresco",
         description:
             "Responsable de la integración de Microsip ERP con Mercado Libre para monitoreo de ventas en tiempo real. Se automatizó el registro de ventas y stock, procesos que antes se realizaban de manera manual.",
-        image: "/assets/images/aresco.png",
+        image: "/assets/images/aresco.webp",
         tags: ["TypeScript", "Java Script", "APIs REST", "Expres.js"],
         link: "https://www.aresco.com.mx/",
         featured: false,
@@ -60,7 +60,7 @@ export const projects = [
         title: "Global Jet Set",
         description:
             "Responsable del desarrollo del sitio web. Resuelve problemas de posicionamiento de marca, embudo de ventas y atención al cliente, brindando una presencia digital profesional que impulsa la captación de clientes en el sector de viajes de lujo.",
-        image: "/assets/images/globaljetset.png",
+        image: "/assets/images/globaljetset.webp",
         tags: ["TypeScript", "React", "TailwindCSS", "Vite"],
         link: "https://globaljetset.mx/",
         featured: false,
@@ -71,7 +71,7 @@ export const projects = [
         title: "Stonehause",
         description:
             "Responsable del desarrollo del sitio web. Resuelve problemas de posicionamiento de marca, embudo de ventas y atención al cliente, fortaleciendo la presencia digital y facilitando el contacto directo con clientes potenciales.",
-        image: "/assets/images/stonehause.png",
+        image: "/assets/images/stonehause.webp",
         tags: ["TypeScript", "React", "TailwindCSS", "Vite"],
         link: "https://stonehaus.com.mx/",
         featured: false,
@@ -82,7 +82,7 @@ export const projects = [
         title: "CareBios",
         description:
             "Responsable del desarrollo del sitio web. Resuelve problemas de posicionamiento de marca, embudo de ventas y atención al cliente, facilitando la captación de clientes y fortaleciendo la presencia digital del negocio.",
-        image: "/assets/images/carebios.png",
+        image: "/assets/images/carebios.webp",
         tags: ["TypeScript", "React", "TailwindCSS", "Vite"],
         link: "https://carebios.com.mx/",
         featured: false,
